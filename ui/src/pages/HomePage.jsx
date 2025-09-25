45db0a563865b0ff3d4b5c1c49840f9fee460c53
snippetarnab <arnabvivo2020@gmail.com>
2025-09-25 20:45:59 +0530

    Update the NoteCard.

--- ui/src/pages/HomePage.jsx
+++ ui/src/pages/HomePage.jsx
@@ -31,7 +31,7 @@ function HomePage() {
     fetchNotes();
   }, []);
   return (
-    <div>
+    <div className="min-h-screen">
       <Navbar />
       {isRateLimited && <RateLimitedUi />}
       <div className="max-w-7xl mx-auto p-4 mt-6">
